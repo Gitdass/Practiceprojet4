6c51c06b6a098b40a7bc6576893e518efb94170a
Gitdass <gkdass6847@gmail.com>
2023-12-14 09:55:08 +0530

    Add project files.

---
+++ Practice4/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practice4.Models;
+
+namespace Practice4.Controllers
+{
+    public class StudentController : Controller
+    {
+        List<Student> student = new List<Student>()
+        {
+            new Student { Id = 1, Name = "dass", Class = "9A", Age = 15, Address = "12 Abc street"},
+            new Student { Id = 2, Name = "vijay", Class = "10B", Age = 16, Address = "15 bell Street"},
+            new Student { Id = 3, Name = "vino", Class = "8A", Age = 14, Address = "567 paris street"},
+        };
+        public IActionResult Index()
+        {
+            return View(student);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Student());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student model)
+        {
+            if (ModelState.IsValid)
+            {
+                student.Add(model);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+    }
+}
---
+++ Practice4/Controllers/TeacherController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practice4.Models;
+
+namespace Practice4.Controllers
+{
+    public class TeacherController : Controller
+    {
+
+        List<Teacher> teacher = new List<Teacher>()
+        {
+            new Teacher { Id = 1, Name = "Mr.Prasath", TeachingSubject = "Physics"},
+            new Teacher { Id = 2, Name = "Mr.varun", TeachingSubject = "Biology"},
+            new Teacher { Id = 3, Name = "Mr.kavin", TeachingSubject = "Maths"},
+        };
+        public IActionResult Index()
+        {
+            return View(teacher);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Teacher());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Teacher model)
+        {
+            if (ModelState.IsValid)
+            {
+                teacher.Add(model);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+    }
+}
